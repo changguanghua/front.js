ad50a2e965a875d9746ba28ea7016342b355fef1
Leo Deng <myst.dg@gmail.com>
2013-05-19 22:18:22 +0800

    add logic

--- front.js
+++ front.js
@@ -1,10 +1,16 @@
 var front = function(source, target, template) {
   var data = Array.isArray(source) ? source : [source];
   var html = "";
+
   data.forEach(function(el) {
-    html += template.replace(/\{\{(\w+)\}\}/g, function($, $1) {
+    var tmpl = template.replace(/@if\{\{(\w+)\}\}((?:(?!@endif)[\s\S])+)@endif/g, function($, $1, $2) {
+      return el[$1] ? $2 : "";
+    });
+
+    html += tmpl.replace(/\{\{(\w+)\}\}/g, function($, $1) {
       return ($1 in el) ? el[$1] : $;
     });
   });
+
   target.innerHTML = html;
 };
